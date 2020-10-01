86b34ef1c9bd37bcdb30ab157c59604145f63d05
Mikhail Matrosov <mikhail.matrosov@gmail.com>
2020-10-01 17:44:23 +0300

    (-) Handle spaces in file names

--- checkers/format_all.sh
+++ checkers/format_all.sh
@@ -1,5 +1,6 @@
+IFS=$'\n';
 for f in $(find $1 -name '*.cpp'); 
 do
     echo Formatting $f ...
-    "c:\Program Files\LLVM\bin\clang-format.exe" -i $f
+    "c:\Program Files\LLVM\bin\clang-format.exe" -i "$f"
 done 
