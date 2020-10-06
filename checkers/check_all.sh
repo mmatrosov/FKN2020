3e0602e05de310368e7608be0ae4ab3469553e6b
Mikhail Matrosov <mikhail.matrosov@gmail.com>
2020-10-06 17:55:25 +0300

    Enhance checker

--- checkers/check_all.sh
+++ checkers/check_all.sh
@@ -1,5 +1,10 @@
+IFS=$'\n';
 for f in $(find $1 -name '*.cpp'); 
 do
     echo Checking $f ...
-    clang-tidy --warnings-as-errors=* -checks=-*,modernize-loop-convert $f -- -std=c++17 -I/usr/include/c++/7 -I/usr/include/x86_64-linux-gnu/c++/7
+    grep "using namespace" "$f" && echo "'using namespace' directive detected!"
+    grep "#define" "$f" && echo "A macro detected!"
+    clang-tidy --warnings-as-errors=* -checks=-*,modernize-loop-convert "$f" -- -std=c++17 -I/usr/include/c++/7 -I/usr/include/x86_64-linux-gnu/c++/7
 done
+
+read -n 1 -s -r -p "Press any key to continue"
